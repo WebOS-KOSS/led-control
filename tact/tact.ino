3446770a348de2f62f14646386c1fb092e1bb3c1
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-07-22 20:30:56 +0900

    함수가 쓰인 순서에 따라서 순서 정렬
    깔끔~

--- tact/tact.ino
+++ tact/tact.ino
@@ -10,25 +10,6 @@ const char* clientName = "ledController"; // client 이름
 
 WiFiClient espClient; // 인터넷과 연결할 수 있는 client 생성
 PubSubClient client(espClient); // 해당 client를 mqtt client로 사용할 수 있도록 설정
-//----------------------------------------------------------------
-
-void setup_wifi() {
-   delay(10);
-   Serial.println();
-   Serial.print("Connecting to ");
-   Serial.println(ssid);
-   WiFi.mode(WIFI_STA);
-   WiFi.begin(ssid, password);
-   while(WiFi.status() != WL_CONNECTED)
-   {
-     delay(500);
-     Serial.print(".");
-   }
-   Serial.println("");
-   Serial.println("WiFi connected");
-   Serial.println("IP address: ");
-   Serial.println(WiFi.localIP()); 
-}
 
 //-----------------------------------------------------------------
 
@@ -52,7 +33,25 @@ int previous4 = LOW;
 int ledPin4 = 1;
 int swPin4 = 15;
 
+//----------------------------------------------------------------
 
+void setup_wifi() {
+   delay(10);
+   Serial.println();
+   Serial.print("Connecting to ");
+   Serial.println(ssid);
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, password);
+   while(WiFi.status() != WL_CONNECTED)
+   {
+     delay(500);
+     Serial.print(".");
+   }
+   Serial.println("");
+   Serial.println("WiFi connected");
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP()); 
+}
 
 //------------------------------------------------------------------
 
@@ -61,22 +60,33 @@ void ledSet(int ledPin, int swPin){
   pinMode(swPin, INPUT_PULLUP); // Tact 스위치 핀 번호 설정
 }
 
-//------------------------------------------------------------------
+//--------------------------------------------------------------------
 
-int ledControl(int ledPin, int swPin, int previous, int ledState, char* ledNum){
-  int reading = digitalRead(swPin);
-  if(reading == HIGH && previous == LOW){//버튼 인식
-    ledState = 1-ledState;
-    char ledMessage[13] = ""; // 문자열을 위한 공간 마련
-    sprintf(ledMessage, "{\"ledState\":%d}", ledState);
-    Serial.print("Publish message: ");
-    Serial.print(ledMessage);
-    Serial.println(ledNum);
-    client.publish(ledNum, ledMessage); // 만든 문자열을 mqtt 서버에 publish *토픽에 숫자 XXX
+void reconnect() {
+  //연결될 때까지 시도
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(clientName))
+    {
+      //연결 성공
+      Serial.println("connected");
+      client.subscribe("subLed1"); // led 토픽 구독
+      client.subscribe("subLed2"); // led 토픽 구독
+      client.subscribe("subLed3"); // led 토픽 구독
+      client.subscribe("subLed4"); // led 토픽 구독
+    } 
+    else 
+    {
+      //연결실패하면 현재 상태 출력하고 5초 후 다시 시도
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
     }
-  digitalWrite(ledPin, ledState);
-  return reading;
+  }
 }
+
 //-------------------------------------------------------------------
 
 void callback(char* topic, byte* payload, unsigned int uLen) {
@@ -114,6 +124,23 @@ void callback(char* topic, byte* payload, unsigned int uLen) {
   } 
 }
 
+//------------------------------------------------------------------
+
+int ledControl(int ledPin, int swPin, int previous, int ledState, char* ledNum){
+  int reading = digitalRead(swPin);
+  if(reading == HIGH && previous == LOW){//버튼 인식
+    ledState = 1-ledState;
+    char ledMessage[13] = ""; // 문자열을 위한 공간 마련
+    sprintf(ledMessage, "{\"ledState\":%d}", ledState);
+    Serial.print("Publish message: ");
+    Serial.print(ledMessage);
+    Serial.println(ledNum);
+    client.publish(ledNum, ledMessage); // 만든 문자열을 mqtt 서버에 publish *토픽에 숫자 XXX
+    }
+  digitalWrite(ledPin, ledState);
+  return reading;
+}
+
 //--------------------------------------------------------------------
 
 void setup(){
@@ -125,36 +152,8 @@ void setup(){
   ledSet(ledPin3, swPin3);
   ledSet(ledPin4, swPin4);
   
-  
   client.setServer(mqtt_server, 1883); //mqtt 서버와 연결(ip, 1883)
   client.setCallback(callback); //callback 함수 세팅
-}
-
-//--------------------------------------------------------------------
-
-void reconnect() {
-  //연결될 때까지 시도
-  while (!client.connected())
-  {
-    Serial.print("Attempting MQTT connection...");
-    if (client.connect(clientName))
-    {
-      //연결 성공
-      Serial.println("connected");
-      client.subscribe("subLed1"); // led 토픽 구독
-      client.subscribe("subLed2"); // led 토픽 구독
-      client.subscribe("subLed3"); // led 토픽 구독
-      client.subscribe("subLed4"); // led 토픽 구독
-    } 
-    else 
-    {
-      //연결실패하면 현재 상태 출력하고 5초 후 다시 시도
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      delay(5000);
-    }
-  }
 }
 
 //--------------------------------------------------------------------
